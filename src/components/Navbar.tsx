07a5c0894c1e8be4af33ef8822f42a658f76d62d
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-04 16:43:38 +0530

    style: :lipstick: large screen navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-center border-b-2 border-b-red-500 uppercase">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-center border-b-2 border-b-red-500 uppercase mb:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Home</Link>
+        <Link href="/">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+
       {/* LOGO */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Flavor Fusion</Link>
       </div>
       {/* MOBILE MENU */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="phone" width={20} height={20} />
+          <span>999 666 333</span>
+        </div>
+        {user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+
+        <CartIcon />
       </div>
     </div>
   );
