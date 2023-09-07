62bc7008d47e74663609b6be312082e800d195e2
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-07 20:38:33 +0530

    refactor: :lipstick: update offer section

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -15,6 +15,7 @@ const Offer = () => {
           of empowerment. Quickly pontificate parallel.
         </p>
         <CountDown />
+
         <button className="bg-red-500 text-white rounded-md py-3 px-6">
           Order Now
         </button>
