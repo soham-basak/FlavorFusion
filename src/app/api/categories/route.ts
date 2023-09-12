1d4889885ecb9a863bdcc5572bd5ccb33c2f6058
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-12 16:55:12 +0530

    feat: :sparkles: categories route & menu page

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// FETCH ALL CATEGORIES 
+export const GET = async () => {
+    
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(
+            JSON.stringify(categories),
+            { status: 200}
+        );
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({message: "Something went wrong!"}), 
+            { status: 500}
+        );
+    }
+};
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,21 @@
-import { menu } from "@/data";
+import { MenuType } from "@/Types/types";
 import Link from "next/link";
 import React from "react";
 
-const MenuPage = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+
+  return res.json();
+};
+
+const MenuPage = async () => {
+  const menu: MenuType = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -16,7 +29,9 @@ const MenuPage = () => {
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
             <p className="text-sm my-8">{category.desc}</p>
             <button
-              className={`hidden 2xl:block bg-black text-white py-2 px-4 rounded-md`}
+              className={`hidden 2xl:block bg-${category.color} text-${
+                category.color === "black" ? "white" : "black"
+              } py-2 px-4 rounded-md`}
             >
               Explore
             </button>
