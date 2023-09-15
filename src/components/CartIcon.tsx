96884359fad979608bb8865f03bcd3e48f628754
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-15 16:26:46 +0530

    feat: :alien: cart

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { useCartStore } from "@/utils/store";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
 const CartIcon = () => {
+  const { totalItems } = useCartStore();
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
   return (
     <Link href="/cart" className="flex items-center gap-4">
       <div className="relative w-8 h-8 md:w-5 md:h-5">
         <Image src="/cart.png" alt="cart" fill />
       </div>
-      <span>CART (3)</span>
+      <span>CART ({totalItems})</span>
     </Link>
   );
 };
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { ProductType } from "@/Types/types";
+import { useCartStore } from "@/utils/store";
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Price = ({ product }: { product: ProductType }) => {
   const [total, setTotal] = useState(product.price);
   const [quantity, setQuantity] = useState(1);
   const [selected, setSelected] = useState(0);
+
+  const { addToCart } = useCartStore();
+
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
 
   useEffect(() => {
     if (product.options?.length) {
@@ -15,6 +23,20 @@ const Price = ({ product }: { product: ProductType }) => {
       );
     }
   }, [quantity, selected, product]);
+
+  const handleCart = () => {
+    addToCart({
+      id: product.id,
+      title: product.title,
+      img: product.img,
+      price: total,
+      ...(product.options?.length && {
+        optionTitle: product.options[selected].title,
+      }),
+      quantity: quantity,
+    });
+    toast.success("The product added to the cart!");
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -56,7 +78,10 @@ const Price = ({ product }: { product: ProductType }) => {
           </div>
         </div>
         {/* CART BUTTON */}
-        <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500">
+        <button
+          className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500"
+          onClick={handleCart}
+        >
           Add to Cart
         </button>
       </div>
