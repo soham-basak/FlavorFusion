7f9a6e30d5c3688c9037c9fdfe9bdbc2b2e437a8
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-16 15:15:29 +0530

    feat: :alien: delete product route

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server'
 import prisma from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
 
 // GET SINGLE PRODUCT
 
@@ -23,4 +24,39 @@ export const GET = async ( req:NextRequest, { params }: { params: { id: string}}
             {status: 500}
         );
     }
+}
+
+// DELETE SINGLE PRODUCT
+
+export const DELETE = async ( req:NextRequest, { params }: { params: { id: string}}) => {
+    const { id } = params;
+
+    const session = await getAuthSession()
+
+    if(session?.user.isAdmin){
+       try {
+        await prisma.product.delete({
+            where: {
+                id:id
+            }, 
+        });
+        return new NextResponse(
+            JSON.stringify("Product deleted!"), 
+            {status: 200}
+        );
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({message: "Something went wrong"}), 
+            {status: 500}
+        );
+    } 
+    } else {
+        return new NextResponse(
+            JSON.stringify({message: "Your are not allowed to delete this item"}), 
+            {status: 403}
+        );
+    }
+
+    
 }
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from "@/Types/types";
+import DeleteButton from "@/components/DeleteButton";
 import Price from "@/components/Price";
 import Image from "next/image";
 import React from "react";
@@ -18,7 +19,7 @@ const getData = async (id: string) => {
 const SingleProductPage = async ({ params }: { params: { id: string } }) => {
   const singleProduct: ProductType = await getData(params.id);
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
+    <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center relative">
       {/* IMAGE CONTAINER */}
       {singleProduct.img && (
         <div className="relative w-full h-1/2 md:h-[70%]">
@@ -38,6 +39,7 @@ const SingleProductPage = async ({ params }: { params: { id: string } }) => {
         <p>{singleProduct.desc}</p>
         <Price product={singleProduct} />
       </div>
+      <DeleteButton id={singleProduct.id} />
     </div>
   );
 };
