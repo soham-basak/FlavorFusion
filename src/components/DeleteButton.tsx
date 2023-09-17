772fa6e8647598bb65fd83f84ff95f2fb2bc37a5
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-17 17:23:19 +0530

    fix: :rocket: bug fixed ready to deploy

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
-const DeleteButton = ({ id }: { id: string }) => {
+const DeleteButton = ({ id }: { id: any }) => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
