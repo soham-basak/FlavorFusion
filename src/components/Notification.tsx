2d697af7124aa4600f05a41839f9aeeda5cfa15e
Soham <71167911+soham-basak@users.noreply.github.com>
2023-09-18 23:52:58 +0530

    Update Notification.tsx

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Notification = () => {
   return (
     <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
-      Free delivery for all orders over $50. Order your food now 🎉🎉
+      Free delivery for all orders over ₹150. Order your food now 🎉🎉
     </div>
   );
 };
